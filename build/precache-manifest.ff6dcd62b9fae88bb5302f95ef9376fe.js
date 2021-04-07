cc553125b6de842e35b1dc60362d844e680933d0
PratyakshMathur <pratyaksh.mathur28@gmail.com>
2021-04-07 14:02:14 +0530

    improving

---
+++ build/precache-manifest.ff6dcd62b9fae88bb5302f95ef9376fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e5dd10473ca7557910207de3230574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c2072190f06af6cdeae",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "87ca56b9ef2ddcbf79da",
+    "url": "/static/css/main.c317ca7f.chunk.css"
+  },
+  {
+    "revision": "0c2072190f06af6cdeae",
+    "url": "/static/js/2.0b0964bb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0b0964bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ca56b9ef2ddcbf79da",
+    "url": "/static/js/main.46de5778.chunk.js"
+  },
+  {
+    "revision": "2223e65c423635d40ef1",
+    "url": "/static/js/runtime-main.adc9b627.js"
+  },
+  {
+    "revision": "d60ee163a02b1f78f6232db3ea6f998f",
+    "url": "/static/media/bcg.d60ee163.jpeg"
+  },
+  {
+    "revision": "796b9d1c20b11cb8f0ec0c1675478376",
+    "url": "/static/media/loadRocket.796b9d1c.gif"
+  }
+]);
